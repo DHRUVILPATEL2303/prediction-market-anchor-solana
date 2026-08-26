87c9d5463737a79cc36964007905f91e5a261438
DHRUVILPATEL2303 <pateldhruvil2303@gmail.com>
2026-08-27 02:03:58 +0530

    feat : mint_yes and mint_no when initialize_amm

--- programs/prediction-market-solana-anchor/src/instructions/initialize_amm.rs
+++ programs/prediction-market-solana-anchor/src/instructions/initialize_amm.rs
@@ -1,38 +1,63 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token};
 
 use crate::{AmmPool, Market, PredictionMarketError};
 
-
-
 #[derive(Accounts)]
 pub struct InitializeAmm<'info> {
-
     #[account(mut)]
-    pub authority : Signer<'info>,
-
+    pub authority: Signer<'info>,
 
     #[account(seeds=[b"market",authority.key().as_ref(), &market.market_id.to_le_bytes()], bump=market.bump,has_one = authority @PredictionMarketError::Unauthorized)]
-    pub market : Account<'info,Market>,
+    pub market: Account<'info, Market>,
 
     #[account(init, payer = authority, space = 8 + AmmPool::INIT_SPACE,seeds = [b"amm",market.key().as_ref()],bump, )]
-    pub amm : Account<'info,AmmPool>,
+    pub amm: Account<'info, AmmPool>,
 
+    ///CHECK : PDA used only as mint authority for YES/NO outcome tokens
+    #[account(seeds=[b"outcome_authority", market.key().as_ref()], bump)]
+    pub outcome_authority: UncheckedAccount<'info>,
 
-    pub system_program : Program<'info,System>,
-    
+    #[account(
+        init,
+        payer = authority,
+        mint::decimals = 6,
+        mint::authority = outcome_authority,
+        mint::freeze_authority = outcome_authority
+    )]
+    pub yes_mint: Account<'info, Mint>,
+
+    #[account(
+        init,
+        payer = authority,
+        mint::decimals = 6,
+        mint::authority = outcome_authority,
+        mint::freeze_authority = outcome_authority
+    )]
+    pub no_mint: Account<'info, Mint>,
+
+    pub token_program: Program<'info, Token>,
+
+    pub system_program: Program<'info, System>,
 }
 
+pub fn initalize_amm(ctx: Context<InitializeAmm>, fee_bps: u16) -> Result<()> {
+    require!(fee_bps <= 1000, PredictionMarketError::InValidFeeBps);
 
-pub fn initalize_amm(ctx : Context<InitializeAmm>, fee_bps : u16) -> Result<()> {
-    require!(fee_bps <=1000, PredictionMarketError::InValidFeeBps);
-    
+
     let amm = &mut ctx.accounts.amm;
     amm.market = ctx.accounts.market.key();
-    amm.no_reserve= 0;
-    amm.yes_reserve=0;
-    amm.lp_supply=0;
-    amm.fee_bps=fee_bps;
-    amm.bump=ctx.bumps.amm;
+    amm.yes_mint = ctx.accounts.yes_mint.key();
+    amm.no_mint = ctx.accounts.no_mint.key();
+    amm.no_reserve = 0;
+    amm.yes_reserve = 0;
+    amm.lp_supply = 0;
+    amm.fee_bps = fee_bps;
+    amm.bump = ctx.bumps.amm;
+
+
+
+    
 
     Ok(())
 }
--- programs/prediction-market-solana-anchor/src/state.rs
+++ programs/prediction-market-solana-anchor/src/state.rs
@@ -60,6 +60,8 @@ pub struct  Position {
 #[derive(InitSpace)]
 pub struct AmmPool {
     pub market : Pubkey ,
+    pub yes_mint : Pubkey,
+    pub no_mint : Pubkey,
     pub yes_reserve : u64, //liquidity reserve for yes
     pub no_reserve : u64,  //liquidity reserve for no
     pub lp_supply: u64,    //total_liquidity_supply shares 
